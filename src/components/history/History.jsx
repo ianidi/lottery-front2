44b4273ea6a33e0883a7b964ad7e78f1ad2b399e
ianidi <51965970+ianidi@users.noreply.github.com>
2021-05-11 21:45:38 +0300

    update

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,5 +1,6 @@
 import { Flex, Grid, Text } from '@chakra-ui/react';
-// import { HistoryItem } from 'components/history/HistoryItem';
+import { BigNumber } from '@ethersproject/bignumber';
+import { HistoryItem } from 'components/history/HistoryItem';
 import { HistoryPagination } from 'components/history/HistoryPagination';
 import { NoHistory } from 'components/history/NoHistory';
 import { useUserHistory } from 'hooks/useUserHistory';
@@ -12,10 +13,11 @@ export const History = ({ page }) => {
   const { transfers, loading } = useUserHistory();
 
   const numPages = Math.ceil(transfers.length / TOTAL_PER_PAGE);
-  const displayHistory = transfers.slice(
-    (page - 1) * TOTAL_PER_PAGE,
-    Math.min(page * TOTAL_PER_PAGE, transfers.length),
-  );
+  // const displayHistory = transfers.slice(
+  //   (page - 1) * TOTAL_PER_PAGE,
+  //   Math.min(page * TOTAL_PER_PAGE, transfers.length),
+  // );
+  const displayHistory = [{ amount: "5", decimals: "8", tokenSymbol: "USDT", timestamp: 1620758121, result: false }];
 
   if (numPages > 1 && page > numPages) {
     return <Redirect to="/history" />;
@@ -39,9 +41,7 @@ export const History = ({ page }) => {
         <>
           <Grid
             templateColumns={{
-              base: '1fr',
-              md: '0.5fr 1.75fr 1fr 1fr 1.25fr 0.5fr',
-              lg: '1fr 1.25fr 1fr 1fr 1.25fr 0.5fr',
+              base: '1fr 1fr 1fr 1fr 1fr',
             }}
             color="grey"
             fontSize="sm"
@@ -49,16 +49,15 @@ export const History = ({ page }) => {
             mb={4}
             display={{ base: 'none', md: 'grid' }}
           >
-            <Text>Date</Text>
-            <Text>Direction</Text>
-            <Text textAlign="center">Sending Tx</Text>
-            <Text textAlign="center">Receiving Tx</Text>
+            <Text textAlign="center">Date</Text>
             <Text textAlign="center">Amount</Text>
-            <Text textAlign="right">Status</Text>
+            <Text textAlign="center">Result</Text>
+            <Text textAlign="center">Game</Text>
+            <Text textAlign="center">Tx</Text>
           </Grid>
-          {/* {displayHistory.map(item => (
-            <HistoryItem key={item.sendingTx} data={item} />
-          ))} */}
+          {displayHistory.map((item, index) => (
+            <HistoryItem key={index} item={item} />
+          ))}
           {numPages > 1 && (
             <HistoryPagination numPages={numPages} currentPage={page} />
           )}
--- src/components/history/HistoryItem.jsx
+++ src/components/history/HistoryItem.jsx
@@ -12,76 +12,12 @@ import RightArrowImage from 'assets/right-arrow.svg';
 import { TxLink } from 'components/common/TxLink';
 import { useWeb3Context } from 'contexts/Web3Context';
 import { BigNumber, utils } from 'ethers';
-import { useBridgeDirection } from 'hooks/useBridgeDirection';
-import {
-  executeSignatures,
-  getMessageFromTxHash,
-  getMessageStatus,
-} from 'lib/amb';
 import { POLLING_INTERVAL } from 'lib/constants';
-import {
-  getExplorerUrl,
-  getNetworkName,
-  logError,
-} from 'lib/helpers';
+import { getExplorerUrl, logError } from 'lib/helpers';
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
-const { formatUnits } = utils;
-
-const shortenHash = hash =>
-  `${hash.slice(0, 6)}...${hash.slice(hash.length - 4, hash.length)}`;
-
-const Tag = ({ bg, txt }) => (
-  <Flex
-    justify="center"
-    align="center"
-    bg={bg}
-    borderRadius="6px"
-    px="0.75rem"
-    height="1.5rem"
-    fontSize="xs"
-    color="white"
-    fontWeight="600"
-    w="auto"
-  >
-    <Text>{txt}</Text>
-  </Flex>
-);
-
-const networkTags = {
-  100: <Tag bg="#4DA9A6" txt="xDai" />,
-  1: <Tag bg="#5A74DA" txt="Ethereum" />,
-  42: <Tag bg="#5A74DA" txt="Kovan" />,
-  77: <Tag bg="#4DA9A6" txt="POA Sokol" />,
-  56: <Tag bg="#5A74DA" txt="BSC" />,
-};
-
-const getNetworkTag = chainId => networkTags[chainId];
-
-export const HistoryItem = ({
-  data: {
-    user,
-    chainId,
-    timestamp,
-    sendingTx,
-    receivingTx: inputReceivingTx,
-    amount,
-    toToken,
-    message: inputMessage,
-  },
-}) => {
-  const {
-    homeChainId,
-    foreignAmbAddress,
-    getBridgeChainId,
-    getMonitorUrl,
-    getGraphEndpoint,
-    enableForeignCurrencyBridge,
-  } = useBridgeDirection();
+export const HistoryItem = ({ item: { amount, result, decimals, tokenSymbol, timestamp } }) => {
   const { providerChainId, ethersProvider } = useWeb3Context();
-  const bridgeChainId = getBridgeChainId(chainId);
-  const [receivingTx, setReceiving] = useState(inputReceivingTx);
-  const [message, setMessage] = useState(inputMessage);
 
   const timestampString = new Date(
     parseInt(timestamp, 10) * 1000,
@@ -93,122 +29,24 @@ export const HistoryItem = ({
     minute: '2-digit',
   });
 
-  const toast = useToast();
-  const showError = useCallback(
-    msg => {
-      if (msg) {
-        toast({
-          title: 'Error',
-          description: msg,
-          status: 'error',
-          isClosable: 'true',
-        });
-      }
-    },
-    [toast],
-  );
-  const claimable = useMemo(
-    () => message && message.msgData && message.signatures,
-    [message],
-  );
-  const [loading, setLoading] = useState(false);
-  const [txHash, setTxHash] = useState();
-  const claimTokens = useCallback(async () => {
-    if (loading) return;
-    if (providerChainId === homeChainId) {
-      showError(`Please switch wallet to ${getNetworkName(foreignChainId)}`);
-    } else if (!claimable) {
-      showError('Still Collecting Signatures...');
-    } else {
-      try {
-        setLoading(true);
-        const tx = await executeSignatures(
-          ethersProvider,
-          foreignAmbAddress,
-          message,
-        );
-        setTxHash(tx.hash);
-      } catch (executeError) {
-        logError({ executeError, chainId: providerChainId, message });
-        if (executeError && executeError.message) {
-          showError(executeError.message);
-        } else {
-          showError(
-            'Impossible to perform the operation. Reload the application and try again.',
-          );
-        }
-      } finally {
-        setLoading(false);
-      }
-    }
-  }, [
-    loading,
-    providerChainId,
-    homeChainId,
-    foreignChainId,
-    ethersProvider,
-    foreignAmbAddress,
-    claimable,
-    message,
-    showError,
-  ]);
-
-  useEffect(() => {
-    const subscriptions = [];
-    const unsubscribe = () => {
-      subscriptions.forEach(s => {
-        clearTimeout(s);
-      });
-    };
-
-    if (receivingTx || !message || !message.msgId) return unsubscribe;
-    let execution = null;
-    let request = null;
-    const { msgId } = message;
-
-    const getStatus = async () => {
-      try {
-        [execution, request] = await Promise.all([
-          !receivingTx
-            ? getMessageStatus(getGraphEndpoint(bridgeChainId), msgId)
-            : null,
-          !message.signatures
-            ? getMessageFromTxHash(getGraphEndpoint(chainId), sendingTx)
-            : null,
-        ]);
-        if (execution) {
-          setReceiving(execution.txHash);
-          setLoading(false);
-          setTxHash();
-        }
-        if (request) {
-          setMessage(request);
-        }
-
-        if (!(receivingTx && message)) {
-          const timeoutId = setTimeout(() => getStatus(), POLLING_INTERVAL);
-          subscriptions.push(timeoutId);
-        }
-      } catch (messageError) {
-        logError({ messageError });
-      }
-    };
-    // unsubscribe from previous polls
-    unsubscribe();
-
-    getStatus();
-    // unsubscribe when unmount component
-    return unsubscribe;
-  }, [
-    chainId,
-    receivingTx,
-    bridgeChainId,
-    message,
-    sendingTx,
-    getGraphEndpoint,
-  ]);
-
-  const { symbol: tokenSymbol } = toToken;
+  // const toast = useToast();
+  // const showError = useCallback(
+  //   msg => {
+  //     if (msg) {
+  //       toast({
+  //         title: 'Error',
+  //         description: msg,
+  //         status: 'error',
+  //         isClosable: 'true',
+  //       });
+  //     }
+  //   },
+  //   [toast],
+  // );
+  // const claimable = useMemo(
+  //   () => message && message.msgData && message.signatures,
+  //   [message],
+  // );
 
   return (
     <Flex
@@ -222,109 +60,31 @@ export const HistoryItem = ({
     >
       <Grid
         templateColumns={{
-          base: '1fr',
-          md: '0.5fr 1.75fr 1fr 1fr 1.25fr 0.5fr',
-          lg: '1fr 1.25fr 1fr 1fr 1.25fr 0.5fr',
+          base: '1fr 1fr 1fr 1fr 1fr',
         }}
         w="100%"
       >
-        <Flex align="center" justify="space-between" mb={{ base: 1, md: 0 }}>
-          <Text display={{ base: 'inline-block', md: 'none' }} color="greyText">
-            Date
-          </Text>
+        <Flex justify="center">
           <Text my="auto">{timestampString}</Text>
         </Flex>
-        <Flex align="center" justify="space-between" mb={{ base: 1, md: 0 }}>
-          <Text display={{ base: 'inline-block', md: 'none' }} color="greyText">
-            Direction
-          </Text>
-          <Flex align="center">
-            {getNetworkTag(chainId)}
-            <Image src={RightArrowImage} mx="0.5rem" />
-            {getNetworkTag(bridgeChainId)}
-          </Flex>
+        <Flex justify="center">
+          <Text my="auto">{utils.formatUnits(BigNumber.from(amount), BigNumber.from(decimals))} {tokenSymbol}</Text>
         </Flex>
-        <Flex
-          align="center"
-          justify={{ base: 'space-between', md: 'center' }}
-          mb={{ base: 1, md: 0 }}
-        >
-          <Text display={{ base: 'inline-block', md: 'none' }} color="greyText">
-            Sending Tx
-          </Text>
-          <Link
-            color="blue.500"
-            href={getMonitorUrl(chainId, sendingTx)}
-            rel="noreferrer noopener"
-            target="_blank"
-            my="auto"
-            textAlign="center"
-          >
-            {shortenHash(sendingTx)}
-          </Link>
+        <Flex justify="center">
+          <Text my="auto">{result ? 'Win' : 'Lose'}</Text>
         </Flex>
-        <Flex
-          align="center"
-          justify={{ base: 'space-between', md: 'center' }}
-          mb={{ base: 1, md: 0 }}
-        >
-          <Text display={{ base: 'inline-block', md: 'none' }} color="greyText">
-            Receiving Tx
-          </Text>
-          {receivingTx ? (
-            <Link
-              color="blue.500"
-              href={`${getExplorerUrl(bridgeChainId)}/tx/${receivingTx}`}
-              rel="noreferrer noopener"
-              target="_blank"
-              my="auto"
-              textAlign="center"
-            >
-              {shortenHash(receivingTx)}
-            </Link>
-          ) : (
-            <Text />
-          )}
+        <Flex justify="center">
+          <Button w="80%" size="sm" colorScheme="blue">View game</Button>
         </Flex>
-        <Flex
-          align="center"
-          justify={{ base: 'space-between', md: 'center' }}
-          mb={{ base: 1, md: 0 }}
-        >
-          <Text display={{ base: 'inline-block', md: 'none' }} color="greyText">
-            Amount
-          </Text>
-          <Text my="auto" textAlign="center">
-            {`${formatUnits(
-              BigNumber.from(amount),
-              toToken.decimals,
-            )} ${tokenSymbol}`}
-          </Text>
+        <Flex justify="center">
+          <Button w="80%" size="sm" colorScheme="blue">View Tx</Button>
         </Flex>
-        {receivingTx ? (
-          <Flex align="center" justify={{ base: 'center', md: 'flex-end' }}>
-            <Image src={BlueTickImage} mr="0.5rem" />
-            <Text color="blue.500">Claimed</Text>
-          </Flex>
-        ) : (
-          <Flex align="center" justify={{ base: 'center', md: 'flex-end' }}>
-            <TxLink
-              chainId={providerChainId}
-              hash={loading ? txHash : undefined}
-            >
-              <Button
-                w="100%"
-                size="sm"
-                colorScheme="blue"
-                onClick={claimTokens}
-                isLoading={loading}
-              >
-                Claim
-              </Button>
-            </TxLink>
-          </Flex>
-        )}
       </Grid>
     </Flex>
   );
 };
+
+{/* <TxLink chainId={providerChainId}></TxLink> */ }
+// hash={txHash}
+// onClick={claimTokens}
+// isLoading={loading}
