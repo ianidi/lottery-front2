6eab3810973a1516b5191ea27b8f3c07695eb3d8
ianidi <51965970+ianidi@users.noreply.github.com>
2021-05-12 14:15:14 +0300

    Updates

---
+++ precache-manifest.9e09f74ae89f40469b972ae5537ff8ca.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8056ff8bf376f3c0b36618b3be9bcf",
+    "url": "/lottery/index.html"
+  },
+  {
+    "revision": "7126d2b99e96be10d12e",
+    "url": "/lottery/static/js/2.d12bf582.chunk.js"
+  },
+  {
+    "revision": "db0791242f8d90e4070706d00dfa0eb0",
+    "url": "/lottery/static/js/2.d12bf582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "437acb9fdb97fdf2d4dc",
+    "url": "/lottery/static/js/main.6a8afa26.chunk.js"
+  },
+  {
+    "revision": "048aa2db7d14089203e9",
+    "url": "/lottery/static/js/runtime-main.9948f010.js"
+  },
+  {
+    "revision": "cad03824c90c6a2de68cd12e7b20b5be",
+    "url": "/lottery/static/media/custom-token.cad03824.svg"
+  },
+  {
+    "revision": "807bb502b1916cee97b8b5caa462ddf9",
+    "url": "/lottery/static/media/down-triangle.807bb502.svg"
+  },
+  {
+    "revision": "d806ed308842745f41c81db5bb66e6a6",
+    "url": "/lottery/static/media/drop-down.d806ed30.svg"
+  },
+  {
+    "revision": "deb1b29038cb91a989fb1a4b2b753652",
+    "url": "/lottery/static/media/search.deb1b290.svg"
+  },
+  {
+    "revision": "b8118c6166f7f4af2754569a860c4c3c",
+    "url": "/lottery/static/media/transfer.b8118c61.svg"
+  },
+  {
+    "revision": "b638e27fafac090b0fd95c2e127503f6",
+    "url": "/lottery/static/media/unlock.b638e27f.svg"
+  },
+  {
+    "revision": "ffb399c804b9bdb832bc3604e22cbc7f",
+    "url": "/lottery/static/media/up-triangle.ffb399c8.svg"
+  }
+]);
